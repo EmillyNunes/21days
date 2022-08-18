0b145b4382decb2455e192af0088e6c292afc37c
emilly <emynunes@outlook.com>
2022-08-17 22:09:23 -0300

    dia 02 - hello world

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  
+  return (
+    <div className="App">
+     <div>
+       <p>
+         Hello World
+       </p> 
+     </div>
+    </div>
+
+  );
+}
+
+export default App;
